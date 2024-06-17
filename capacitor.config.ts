f95acdcf8d579c819c5994d9eb72cb2933bac851
JosephLucaSanJuan <jurassicjoe03@gmail.com>
2024-06-17 08:45:04 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'examen_recuperacion_siete_y_media',
+  webDir: 'www'
+};
+
+export default config;
